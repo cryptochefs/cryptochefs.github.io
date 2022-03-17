3374b44f0857c02e8c6c2fd498b7202ede28e1a7
Simon Buechi <simon.buechi@gmail.com>
2022-03-17 13:43:26 +0100

    add internal polygon admin page

---
+++ internal-polygon/static/js/16.967916c3.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpapp=this.webpackJsonpapp||[]).push([[16],{1927:function(n,p){},1935:function(n,p){},1945:function(n,p){},1951:function(n,p){}}]);
+//# sourceMappingURL=16.967916c3.chunk.js.map
